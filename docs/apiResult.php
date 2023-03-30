e41446a0770e345ccce71ab9245f1e5c5f04a45b
chenzikangyoder@gmail.com <chenzikangyoder@gmail.com>
2023-03-30 12:03:58 +0800

    a

--- docs/apiResult.php
+++ docs/apiResult.php
@@ -2,13 +2,13 @@
 error_reporting(0);
 
 $version = "1.1";
-$versionIntro = "百利甜了咩"; //支持html代码
+$versionIntro = "<p>百利甜了咩</p>"; //支持html代码
 $versionUrl = "";
 $versionCode = 11;
 
 //公告，支持html代码
 
-$announcement = "test";
+$announcement = "<p>test</p>";
 
 
 if(isset($_GET['update'])){
