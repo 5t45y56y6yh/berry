ee591a1442c2ecf0f44a306671012cf5c3081011
5t45y56y6yh <101694378+5t45y56y6yh@users.noreply.github.com>
2023-03-31 16:24:31 +0800

    Update apiResult.php

--- docs/apiResult.php
+++ docs/apiResult.php
@@ -1,6 +1,6 @@
 {
     "version":"V1.3.1",
-    "versionIntro":"通知权限大家自己开下还是不会弄",
+    "versionIntro":"通知权限自己开下还是不会弄",
     "versionUrl":"https://www.pgyer.com/0YDv",
     "versionCode":131,
     "announcement":"每月20-30停机"
